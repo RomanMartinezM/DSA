dcee807a7705489ea79c091fcf0c954ee69b2215
ronmar <sigma4726spring@gmail.com>
2026-01-12 22:29:14 -0600

    feat: Adding strobogrammatic problem

---
+++ two-pointers/strobogrammatic.py
@@ -0,0 +1,37 @@
+def is_strobogrammatic(num):
+    mapping = {
+        '0' : '0',
+        '1' : '1',
+        '8' : '8',
+        '6' : '9',
+        '9' : '6'
+    }
+
+    left, right = 0, len(num) - 1
+
+    while left <= right:
+        if num[left] not in mapping or mapping[num[left]] != num[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+def main():
+    test_cases = [
+        "609",   
+        "88",   
+        "962",  
+        "101",  
+        "123"   
+    ]
+
+    for i, num in enumerate(test_cases):
+        print(i + 1, ".\tnum: ", num, sep="")
+        print("\n\tIs strobogrammatic: ", is_strobogrammatic(num), sep="")
+        print("-" * 50)
+
+if __name__ == "__main__":
+    main()
